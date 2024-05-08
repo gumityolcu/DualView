2ebaa34e0552debd0f5476a1dfe1a10de0f3b00a
Galip Ümit Yolcu <yolcu@vca-gpu-headnode.fe.hhi.de>
2024-05-08 17:20:32 +0200

    delete slurm

--- slurm/basic_explain.sh
+++
@@ -1,5 +0,0 @@
-for file in ../config_files/cluster/explain/$1/basic*.yaml; do
-    echo $file
-    sbatch explain_job.sh $file
-    sleep 1
-done;
--- slurm/train.sh
+++
@@ -1,5 +0,0 @@
-for file in ../config_files/cluster/train/$1/*.yaml; do
-    echo $file
-    sbatch train_job_$1.sh $file
-    sleep 1
-done;
