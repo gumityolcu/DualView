da8c963dfffaea94b377955cc23e80fb312d0b89
Galip Ümit Yolcu <galip.uemit.yolcu@hhi.fraunhofer.de>
2024-11-18 16:34:18 +0100

    add sklearn wrapper for dualview surrogate

--- src/explainers/dualview.py
+++ src/explainers/dualview.py
@@ -5,8 +5,59 @@ import time
 from copy import deepcopy
 from utils.csv_io import read_matrix, write_data
 from utils.explainers import FeatureKernelExplainer
+from sklearn.svm import LinearSVC ##modified LIBLINEAR MCSVM_CS_Solver which returns the dual variables
+
 
 class DualView(FeatureKernelExplainer):
+    name = "DualViewExplainer"
+    def __init__(self, model, dataset, device, dir, C=1.0, max_iter=50000, normalize=False):
+        super().__init__(model, dataset, device, normalize=normalize)
+        self.C=C
+        if dir[-1]=="\\":
+            dir=dir[:-1]
+        self.dir=dir
+        self.features_dir=dir
+        self.max_iter=max_iter
+        os.makedirs(self.dir, exist_ok=True)
+        os.makedirs(self.features_dir, exist_ok=True)
+
+    def read_variables(self):
+        self.learned_weight = torch.load(os.path.join(self.dir,"weights"), map_location=self.device).to(torch.float)
+        self.coefficients=torch.load(os.path.join(self.dir,"dualview_coefficients"), map_location=self.device).to(torch.float)
+        self.train_time=torch.load(os.path.join(self.dir,"train_time"), map_location=self.device).to(torch.float)
+
+    def train(self):
+        tstart = time.time()
+        
+        if not os.path.isfile(os.path.join(self.features_dir, "samples")):
+            torch.save(self.normalized_samples,os.path.join(self.features_dir, "samples"))
+        if not os.path.isfile(os.path.join(self.features_dir, "labels")):
+            torch.save(self.labels,os.path.join(self.features_dir, "labels"))
+        
+        if os.path.isfile(os.path.join(self.dir,'weights')) and os.path.isdir(os.path.join(self.dir,'coefficients')):
+            self.read_variables()
+        else:
+            model = LinearSVC(multi_class="crammer_singer", max_iter=self.max_iter, C=self.C)
+            model.fit(self.normalized_samples.cpu(),self.labels.cpu())
+
+            self.coefficients=torch.tensor(model.alpha_.T,dtype=torch.float,device=self.device)
+            self.learned_weight=torch.tensor(model.coef_,dtype=torch.float, device=self.device)
+            self.train_time = torch.tensor(time.time() - tstart)
+
+            torch.save(self.train_time,os.path.join(self.dir,'train_time'))
+            torch.save(self.learned_weights,os.path.join(self.dir,'weights'))
+            torch.save(self.coefficients,os.path.join(self.dir,'coefficients'))
+            print(f"Training took {self.train_time} seconds")
+        return self.train_time
+
+    def self_influences(self, only_coefs=False):
+        self_coefs=super().self_influences()
+        if only_coefs:
+            return self_coefs
+        else:
+            return self.normalized_samples.norm(dim=-1)*self_coefs
+
+class DualView_Shark(FeatureKernelExplainer):
     name = "DualViewExplainer"
     def __init__(self, model, dataset, device, dir, C=1.0, normalize=True):
         if os.path.isfile(os.path.join(dir,"labels_tensor")) and os.path.isfile(os.path.join(dir,"samples_tensor")):
